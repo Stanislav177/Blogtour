42571d0e80d82b4fe5951424c619e15d87dbde13
Stanislav <stas.177@mail.ru>
2022-12-03 13:25:13 +0300

    добавлен стартовый экран

---
+++ app/src/main/java/com/example/blogtour/ui/home/HomeFragment.kt
@@ -0,0 +1,43 @@
+package com.example.blogtour.ui.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.example.blogtour.appState.AppStateListBlog
+import com.example.blogtour.databinding.FragmentHomeBinding
+import com.example.blogtour.utils.BaseFragment
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    private val postViewModel: HomeViewModel by lazy {
+        ViewModelProvider(this)[HomeViewModel::class.java]
+    }
+
+    private val adapter: HomeRecyclerAdapter by lazy {
+        HomeRecyclerAdapter()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        postViewModel.getLiveData().observe(viewLifecycleOwner) {
+            renderData(it)
+        }
+        recyclerListBlog.adapter = adapter
+
+        postViewModel.getPostList()
+    }
+
+    private fun renderData(it: AppStateListBlog) {
+        when (it) {
+            is AppStateListBlog.Error -> {
+
+            }
+            is AppStateListBlog.Success -> {
+                adapter.setPostData(it.dataPost)
+            }
+        }
+    }
+
+}
