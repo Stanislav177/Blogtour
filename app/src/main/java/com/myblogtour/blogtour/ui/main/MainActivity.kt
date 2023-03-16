556206e9f519e375d752f3860b9c1264813a7336
Stanislav <stas.177@mail.ru>
2023-03-16 22:43:50 +0300

    добавил возможность менять тему на светлую или темную из настроек

--- app/src/main/java/com/myblogtour/blogtour/ui/main/MainActivity.kt
+++ app/src/main/java/com/myblogtour/blogtour/ui/main/MainActivity.kt
@@ -3,12 +3,17 @@ package com.myblogtour.blogtour.ui.main
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate.*
 import androidx.fragment.app.Fragment
 import com.myblogtour.blogtour.R
 import com.myblogtour.blogtour.appState.AppStateMainActivity
 import com.myblogtour.blogtour.databinding.ActivityMainBinding
 import com.myblogtour.blogtour.ui.home.HomeFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
+const val SYSTEM_THEME = 0
+const val LIGHT_THEME = 1
+const val NIGHT_THEME = 2
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,16 +22,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startTheme(getThemeSP())
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         if (savedInstanceState == null) {
             toFragment(HomeFragment())
         }
-        initAppBar()
+        initBottomAppBar()
     }
 
-    private fun initAppBar() {
+    private fun initBottomAppBar() {
         with(binding) {
             btnHomeFab.setOnClickListener {
                 toFragment(HomeFragment())
@@ -64,5 +71,27 @@ class MainActivity : AppCompatActivity() {
     private fun toFragment(f: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.containerFragment, f).commit()
+    }
+
+    private fun startTheme(theme: Int) {
+        return when (theme) {
+            LIGHT_THEME -> {
+                setDefaultNightMode(MODE_NIGHT_NO)
+            }
+            NIGHT_THEME -> {
+                setDefaultNightMode(MODE_NIGHT_YES)
+            }
+            SYSTEM_THEME -> {
+                setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+            else -> {
+                setDefaultNightMode(MODE_NIGHT_FOLLOW_SYSTEM)
+            }
+        }
+    }
+
+    private fun getThemeSP(): Int {
+        val sharedPreferences = getSharedPreferences("KEY_SP_THEME", MODE_PRIVATE)
+        return sharedPreferences.getInt("KEY_THEME", 0)
     }
 }
