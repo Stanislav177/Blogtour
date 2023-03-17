7fc030050f26fc976a61609408bf28398d7d321b
Stanislav <stas.177@mail.ru>
2023-03-17 18:16:35 +0300

    создан bottomSheetFragment для поиска и фрагмент отображения результата поиска

---
+++ app/src/main/java/com/myblogtour/blogtour/ui/search/ResultSearchFragment.kt
@@ -0,0 +1,10 @@
+package com.myblogtour.blogtour.ui.search
+
+import com.myblogtour.blogtour.databinding.FragmentResultSearchBinding
+import com.myblogtour.blogtour.utils.BaseFragment
+
+class ResultSearchFragment :
+    BaseFragment<FragmentResultSearchBinding>(FragmentResultSearchBinding::inflate) {
+
+
+}
---
+++ app/src/main/java/com/myblogtour/blogtour/ui/search/ResultSearchViewModel.kt
@@ -0,0 +1,6 @@
+package com.myblogtour.blogtour.ui.search
+
+import androidx.lifecycle.ViewModel
+
+class ResultSearchViewModel : ViewModel() {
+}
