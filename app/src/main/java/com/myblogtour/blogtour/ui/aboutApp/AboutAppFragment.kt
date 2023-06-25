e9f6a53e0f06db5cbf620e259b86e051fb8fe032
Stanislav <stas.177@mail.ru>
2023-06-25 20:21:12 +0300

    создан фрагмент о приложении

---
+++ app/src/main/java/com/myblogtour/blogtour/ui/aboutApp/AboutAppFragment.kt
@@ -0,0 +1,9 @@
+package com.myblogtour.blogtour.ui.aboutApp
+
+import com.myblogtour.blogtour.databinding.FragmentAboutAppBinding
+import com.myblogtour.blogtour.utils.BaseFragment
+
+class AboutAppFragment : BaseFragment<FragmentAboutAppBinding>(FragmentAboutAppBinding::inflate) {
+
+
+}
